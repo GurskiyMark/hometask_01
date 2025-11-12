4860ba21104d67272f54d771f397f6c1e69d2f50
GurskiyMark <kotempu@gmail.com>
2025-11-13 00:05:50 +0300

    fix vercel ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 import { videosRouter } from './routes/videosRouter';
 
-export const app = express();
+const app = express();
 app.use(express.json());
 
+// Все роуты
 app.use('/hometask_01/api/videos', videosRouter);
+
+// Корень можно просто вернуть что-то
+app.get('/', (req, res) => res.send('API работает'));
+
+export { app };
